14c860616536684a89a3357f45fae0eb8ba63a7c
trevorbagels <therealangrybagel@gmail.com>
2022-08-09 12:23:06 -0700

    spiced up the UI, added authentication.

--- dev/main.py
+++ dev/main.py
@@ -1,3 +1,4 @@
+import dotenv
 from flask import Flask, Response, request
 from flask_httpauth import HTTPBasicAuth
 from flask_restful import Resource, Api, reqparse
@@ -7,7 +8,14 @@ from .prodict import Prodict
 
 from . import utils
 from . import core
-import os, requests, time, urllib.parse, threading
+import os, requests, time, urllib.parse, threading, hashlib, json, os, random
+
+from itsdangerous import BadSignature, SignatureExpired
+from itsdangerous import TimedJSONWebSignatureSerializer
+
+from dotenv import load_dotenv
+
+auth = HTTPBasicAuth()
 
 HOST = "0.0.0.0"
 PORT = "5001"
@@ -43,17 +51,20 @@ class Main:
 
 
 	def format_result_string(self, x:core.Result) -> str:
-		s = "/r/" + x.subreddit + "<br>"
+		s = "<b>/r/" + x.subreddit + "</b><br><div>"
 		s += str( x.total_comments + x.total_posts )
 		if x.total_comments_exceeds or x.total_posts_exceeds: s += "+"
 		s += " " + utils.pluralize("match", x.total_comments + x.total_posts, "matches", include_value=False)
 		if x.total_comments + x.total_posts > 0:
 			s += "<br>" + utils.pluralize(str(x.total_posts), int(x.total_posts_exceeds) + 1, str(x.total_posts) + "+", include_value=False) + " " + utils.pluralize("post", x.total_posts, "posts", include_value=False)
 			s += "<br>" + utils.pluralize(str(x.total_comments), int(x.total_comments_exceeds) + 1, str(x.total_comments) + "+", include_value=False) + " " + utils.pluralize("comment", x.total_comments, "comments", include_value=False)
+		s += "</div>"
+		if x.total_comments + x.total_posts > 0: s = "<div class='redborder'>" + s + "</div>"
+		else: s = "<div>" + s + "</div>"
 		return s
 
 	def get_subreddit_user_data(self, username, subreddit) -> core.Result:
-
+		time.sleep(.25)
 		result = None
 
 		collect_data = True
@@ -66,26 +77,31 @@ class Main:
 			collect_data = False #we already ran a scan on this subreddit
 
 		while collect_data == True: #break this loop if we don't get ratelimited
-			params = {"author": username, "subreddit": subreddit, "size": self.cfg.max_results}
-			r_submissions = requests.get("https://api.pushshift.io/reddit/search/submission/?" + urllib.parse.urlencode(params))
-			time.sleep(self.cfg.request_delay)
-			r_comments = requests.get("https://api.pushshift.io/reddit/search/comment/?" + urllib.parse.urlencode(params))
-			time.sleep(self.cfg.request_delay)
+			try:
+				params = {"author": username, "subreddit": subreddit, "size": self.cfg.max_results}
+				r_submissions = requests.get("https://api.pushshift.io/reddit/search/submission/?" + urllib.parse.urlencode(params))
+				time.sleep(self.cfg.request_delay)
+				r_comments = requests.get("https://api.pushshift.io/reddit/search/comment/?" + urllib.parse.urlencode(params))
+				time.sleep(self.cfg.request_delay)
 
-			if r_submissions.status_code == 429 or r_comments.status_code == 429:
-					print("ratelimited!")
-					time.sleep(self.cfg.ratelimit_wait)
-					continue
-			elif r_submissions.status_code != 200 or r_comments.status_code != 200: #in case of error
-				print("Something went wrong... (get user subreddit data requests)")
-				print(r_submissions.status_code, r_comments.status_code)
-				#print(r_submissions.url, r_submissions.status_code, "\n- - -\n", r_comments.url, r_comments.status_code, "\n- - -\n", r_submissions.content, "\n\n", r_comments.content)
-				break
-			else: #break after this
-				sr = self.users[username].subreddits[subreddit]
-				for x in r_submissions.json()["data"]: sr.posts.append(core.Post.from_dict(x))
-				for x in r_comments.json()["data"]: sr.comments.append(core.Comment.from_dict(x))
-				break
+				if r_submissions.status_code == 429 or r_comments.status_code == 429:
+						print("ratelimited!")
+						time.sleep(self.cfg.ratelimit_wait)
+						continue
+				elif r_submissions.status_code != 200 or r_comments.status_code != 200: #in case of error
+					print("Something went wrong... (get user subreddit data requests)")
+					print(r_submissions.status_code, r_comments.status_code)
+					#print(r_submissions.url, r_submissions.status_code, "\n- - -\n", r_comments.url, r_comments.status_code, "\n- - -\n", r_submissions.content, "\n\n", r_comments.content)
+					break
+				else: #break after this
+					sr = self.users[username].subreddits[subreddit]
+					for x in r_submissions.json()["data"]: sr.posts.append(core.Post.from_dict(x))
+					for x in r_comments.json()["data"]: sr.comments.append(core.Comment.from_dict(x))
+					break
+			except:
+				time.sleep(3)
+				print("something went wrong... trying again.", subreddit)
+				continue
 		
 		sr = self.users[username].subreddits[subreddit]
 		#create result
@@ -108,7 +124,8 @@ class SubredditList(Resource):
 		self.app = app
 	
 	def get(self):
-		#! ADD AUTHORIZATION
+		if self.app.verify() == False:
+			return "Unauthorized"
 		return self.main.cfg.sr_list.split(" ")
 
 
@@ -118,6 +135,9 @@ class SubredditUserData(Resource):
 		self.app = app
 	
 	def get(self):
+		if self.app.verify() == False:
+			return "Unauthorized"
+
 		parser = reqparse.RequestParser()
 		parser.add_argument("username", type=str, required=True)
 		parser.add_argument("subreddit", type=str, required=True)
@@ -126,19 +146,78 @@ class SubredditUserData(Resource):
 		return r
 
 
+class Authorized(Resource):
+	def __init__(self, main:Main, app):
+		self.main = main
+		self.app = app
+	
+	def get(self):
+		if self.app.verify() == False:
+			return "Unauthorized"
+		else:
+			return "Authorized"
+
+class SetPassword(Resource):
+	def __init__(self, main:Main, app):
+		self.main = main
+		self.app = app
+	
+	def post(self):
+		if self.app.verify() == False:
+			return "Unauthorized"
+		
+		parser = reqparse.RequestParser()
+		parser.add_argument('original', type=str, required=True)
+		parser.add_argument('new', type=str, required=True)
+		args = parser.parse_args()
+		if self.app.get_pwd_hash(args.original) == os.environ.get("pass"):
+			dotenv.set_key(".env", "pass", self.app.get_pwd_hash(args.new))
+			return "<b style='color:lightgreen;'>Password set.</b>"
+		else:
+			return "<b style='color:lightred;'>Unauthorized or invallid password</b>"
+
+class GenerateToken(Resource):
+	def __init__(self, main:Main, app):
+		self.main = main
+		self.app = app
+
+	def get(self): #gets a token
+		parser = reqparse.RequestParser()
+		parser.add_argument('password', required=True)
+		args = parser.parse_args()
+		password = args.password
+		
+		pwd_hash = self.app.get_pwd_hash(password)
+
+		if os.environ.get("pass") == pwd_hash:
+			s = TimedJSONWebSignatureSerializer(self.app.app.config["SECRET_KEY"], expires_in=60*60*24)
+			token = s.dumps({}).decode()
+			return token
+		else:
+			print("Unauthorized access attempt")
+			return None
 
 
 class FlaskAPI:
 	def __init__(self, main):
+		load_dotenv()
 		self.main = main
 		self.app = Flask(__name__)
-		self.app.config["SECRET_KEY"] = "yes"
+		self.app.config["SECRET_KEY"] = "0789QV0O-KPL,H297384TRUIOBFUWhIASLC7TYVOUgsid"
 
 		api = Api(self.app)
 		cors = CORS(self.app)
 		self.app.config["CORS_HEADERS"] = "Content-Type"
 		api.add_resource(SubredditList, "/subredditlist", resource_class_kwargs={'main': self.main, 'app': self})
 		api.add_resource(SubredditUserData, "/subreddituserdata", resource_class_kwargs={'main': self.main, 'app': self})
+		
+		api.add_resource(Authorized, "/checkauth", resource_class_kwargs={'main': self.main, 'app': self})
+		api.add_resource(SetPassword, "/setpass", resource_class_kwargs={'main': self.main, 'app': self})
+		api.add_resource(GenerateToken, "/gettoken", resource_class_kwargs={'main': self.main, 'app': self})
+
+		if os.environ.get("pass") == None:
+			pwd = self.get_pwd_hash("defaultpass")
+			dotenv.set_key(".env", "pass", pwd)
 
 		#start api
 		kwargs = {"host": HOST, "port": PORT}
@@ -146,3 +225,29 @@ class FlaskAPI:
 			kwargs = {}
 		#threading.Thread(target=self.app.run, kwargs=kwargs).start()
 		self.app.run(**kwargs)
+	
+
+	def get_pwd_hash(self, txt):
+		salt = os.environ.get("salt")
+		if salt == None:
+			salt = random.randint(-99999999, 99999999)
+			dotenv.set_key('.env', 'salt', str(salt))
+		print(salt, txt)
+		p = hashlib.pbkdf2_hmac('sha256', str(txt).encode(), str(salt).encode(), 110000).hex()
+		print(p)
+		return p
+
+	def verify(self): #verify access token
+		parser = reqparse.RequestParser()
+		parser.add_argument('token', required=True)
+		args = parser.parse_args()
+		token = args.token
+		s = TimedJSONWebSignatureSerializer(self.app.config["SECRET_KEY"])
+		try:
+			data = s.loads(token.encode())
+		except SignatureExpired:
+			return False #token is expired
+		except BadSignature:
+			return False #token never existed
+		
+		return True
